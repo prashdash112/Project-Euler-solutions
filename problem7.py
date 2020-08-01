480b1d7aaed0473dc41d76ff3e4f205208037f73
Prashant singh <prashdash112@gmail.com>
2020-08-02 00:08:10 +0530

    Update problem7.py

--- problem7.py
+++ problem7.py
@@ -1,3 +1,14 @@
+'''
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+
+
+'''
+
+
+
 p_no = 0
 
 def is_prime(x):
@@ -12,12 +23,11 @@ def is_prime(x):
 	        
 
 arr=[]
-for i in range(int(15)):
+for i in range(int(100000)):
     if is_prime(i)==True:
         p_no += 1
         arr.append(i)
 print(arr)
-print(arr[3])
 print ("We found " + str(p_no) + " prime numbers.") 
 
 
